910b2831a941033c20d536b1fe080bf64d7ad692
Jacob Grunwald <jacob.grunwald@colorado.edu>
2017-03-06 13:43:17 -0700

    added framework for all cmocka tests

--- Project2/Sources/main.c
+++ Project2/Sources/main.c
@@ -31,19 +31,11 @@
 #include "MKL25Z4.h"
 
 static int i = 0;
-
+/*
 int main(void)
 {
 
-    /* Write your code here */
-
-    /* This for loop should be replaced. By default this loop allows a single stepping. */
-    for (;;) {
-        i++;
-    }
-    /* Never leave main */
-    return 0;
-}
+}*/
 ////////////////////////////////////////////////////////////////////////////////
 // EOF
 ////////////////////////////////////////////////////////////////////////////////
--- Project2/Sources/test.c
+++ Project2/Sources/test.c
@@ -2,11 +2,12 @@
 #include <stdarg.h>
 #include <setjmp.h>
 #include <stdlib.h>
-#include "/usr/include/cmocka.h"
+#include <cmocka.h>
 
 #include "circbuf.h"
 #include "data.h"
 #include "memory.h"
+#include "defines.h"
 /*
 #include "mock_circbuf.h"
 #include "mock_data.h"
@@ -15,7 +16,7 @@
 
 #include <stdint.h>
 
-static void test_invalid_pointer_mem() {
+static void test_invalid_pointer_memmove(void **state) {
   uint8_t * src = NULL;
   uint8_t dst[] = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE};
   uint32_t length = 3;
@@ -27,13 +28,175 @@ static void test_invalid_pointer_mem() {
 
   int ret2 = (int) my_memmove(src, ndst, length);
 
-  assert_int_equal(ret1, -1);
-  assert_int_equal(ret2, -1);
+  assert_int_equal(ret1, PTR_ERROR);
+  assert_int_equal(ret2, PTR_ERROR);
+}
+
+static void test_overlap_memmove(void **state) {
+  uint8_t src[] = {0xAA, 0xBB, 0xCC, 0xDD};
+  uint8_t dst[] = {0xEE,0xFF,0x00,0x11};
+  uint32_t length = 4;
+
+  int ret = (int) my_memmove(src, dst, length);
+  uint32_t i;
+  assert_int_equal(ret, SUCCESS);
+  for(i = 0; i < length; i ++) {
+    assert_true(dst[i] == src[i]);
+  }
+}
+
+static void test_SRC_DST_overlap_memmove(void **state) {
+  uint8_t src[] = {0xAA, 0xBB, 0xCC, 0xDD};
+  uint8_t * dst = src - 1;
+  dst[0] = 0x99;
+  uint32_t length = 4;
+  // Need temporary holder to compare destination to as src will be overwritten
+  uint8_t * hld = src;
+
+  int ret = (int) my_memmove(src, dst, length);
+  uint32_t i;
+  assert_int_equal(ret, SUCCESS);
+  for(i = 0; i < length; i ++) {
+    assert_true(dst[i] == hld[i]);
+  }
+}
+
+static void test_DST_SRC_overlap_memmove(void **state) {
+  uint8_t src[] = {0xAA, 0xBB, 0xCC, 0xDD};
+  uint8_t * dst = src + 1;
+  dst[3] = 0xEE;
+  uint32_t length = 4;
+  // Need temporary holder to compare destination to as src will be overwritten
+  uint8_t * hld = src;
+
+  int ret = (int) my_memmove(src, dst, length);
+  uint32_t i;
+  assert_int_equal(ret, SUCCESS);
+  for(i = 0; i < length; i ++) {
+    assert_true(dst[i] == hld[i]);
+  }
+}
+
+static void test_invalid_pointer_memset(void **state) {
+
+}
+
+static void test_check_set_memset(void **state) {
+
+}
+
+static void test_invalid_pointer_memzero(void **state) {
+
+}
+
+static void test_check_set_memzero(void **state) {
+
+}
+
+static void test_invalid_pointer_reverse(void **state) {
+
+}
+
+static void test_odd_reverse(void **state) {
+
+}
+
+static void test_even_reverse(void **state) {
+
+}
+
+static void test_check_characters_reverse(void **state) {
+
+}
+
+/**********************************************/
+
+static void test_invalid_pointer_BtL(void **state) {
+
+}
+
+static void test_valid_converstion_BtL(void **state) {
+
+}
+
+static void test_invalid_ptr_LtB(void **state) {
+
+}
+
+static void test_valid_conversion_LtB(void **state) {
+
+}
+
+/************************************************/
+
+static void test_allocate_free(void **state) {
+
+}
+
+static void test_invalid_pointer_circbuf(void **state) {
+  // Test all of the functions for invalid pointers.
+}
+
+static void test_non_init_buff(void **state) {
+
+}
+
+static void test_add_remove(void **state) {
+
+}
+
+static void test_buffer_full(void **state) {
+
+}
+
+static void test_buffer_empty(void **state) {
+
+}
+
+static void test_wrap_add(void **state) {
+
+}
+
+static void test_wrap_remove(void **state) {
+
+}
+
+static void test_over_fill(void **state) {
+
+}
+
+static void test_over_empty(void **state) {
+
 }
 
 int main() {
   const struct CMUnitTest tests[] = {
-    cmocka_unit_test(test_invalid_pointer_mem),
+    cmocka_unit_test(test_invalid_pointer_memmove),
+    cmocka_unit_test(test_overlap_memmove),
+    cmocka_unit_test(test_SRC_DST_overlap_memmove),
+    cmocka_unit_test(test_DST_SRC_overlap_memmove),
+    cmocka_unit_test(test_invalid_pointer_memset),
+    cmocka_unit_test(test_check_set_memset),
+    cmocka_unit_test(test_invalid_pointer_memzero),
+    cmocka_unit_test(test_check_set_memzero),
+    cmocka_unit_test(test_invalid_pointer_reverse),
+    cmocka_unit_test(test_odd_reverse),
+    cmocka_unit_test(test_even_reverse),
+    cmocka_unit_test(test_check_characters_reverse),
+    cmocka_unit_test(test_invalid_pointer_BtL),
+    cmocka_unit_test(test_valid_converstion_BtL),
+    cmocka_unit_test(test_invalid_ptr_LtB),
+    cmocka_unit_test(test_valid_conversion_LtB),
+    cmocka_unit_test(test_allocate_free),
+    cmocka_unit_test(test_invalid_pointer_circbuf),
+    cmocka_unit_test(test_non_init_buff),
+    cmocka_unit_test(test_add_remove),
+    cmocka_unit_test(test_buffer_full),
+    cmocka_unit_test(test_buffer_empty),
+    cmocka_unit_test(test_wrap_add),
+    cmocka_unit_test(test_wrap_remove),
+    cmocka_unit_test(test_over_fill),
+    cmocka_unit_test(test_over_empty),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
